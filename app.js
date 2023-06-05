89f8873ea5af092e946732592840574a1a9a8328
jeetuisbest <jeetuchaudhari786@gmail.com>
2023-06-05 18:36:05 +0530

    try again

--- app.js
+++ app.js
@@ -25,29 +25,29 @@ app.listen(port, () => {
     try {
 
         // https://graph.facebook.com/v12.0/100819983038758/messages?access_token=EAAVFkSD0htMBAGebAC0k1koHwEnR6XEUwPGS2HqTjZAlSmybdBHeAEXeGiMZAwzCzbMtjr65QTlExcfsAi2fLYKCU5Jd2tAdxViUeokf7CEyyWMUh0qVfSBEuvhaZBuqoZCnKYOlnMZCtKA9PqVEoOlSWGz5D36nEaCDsg45alHvjQS6FzALyiwZBjJoscFwzv9Q72UFXosQZDZD
-        // axios({
-        //     method: "POST",
-        //     url: "https://graph.facebook.com/v12.0/100819983038758/messages?access_token=" + token,
-        //     data: {
-        //         messaging_product: "whatsapp",
-        //         recipient_type: "individual",
-        //         to: "918080313557",
-        //         type: "template",
-        //         template: {
-        //             name: "hello_world",
-        //             language: {
-        //                 "code": "en_US"
-        //             }
-        //         }
-        //     },
-        //     headers: { "Content-Type": "application/json" }
-        // })
-        //     .then(response => {
-        //         console.log("Axios request successful:", response.data);
-        //     })
-        //     .catch(error => {
-        //         console.log("Axios request failed:", error);
-        //     });
+        axios({
+            method: "POST",
+            url: "https://graph.facebook.com/v12.0/100819983038758/messages?access_token=" + token,
+            data: {
+                messaging_product: "whatsapp",
+                recipient_type: "individual",
+                to: "918080313557",
+                type: "template",
+                template: {
+                    name: "hello_world",
+                    language: {
+                        "code": "en_US"
+                    }
+                }
+            },
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => {
+                console.log("Axios request successful:", response.data);
+            })
+            .catch(error => {
+                console.log("Axios request failed:", error);
+            });
 
 
     } catch (error) {
